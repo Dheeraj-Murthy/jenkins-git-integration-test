3928da31c8d8189b5f0e0757fb05ef7444c6ae72
M S Dheeraj Murthy <136881238+Dheeraj-Murthy@users.noreply.github.com>
2026-08-17 09:05:21 +0530

    Update calc.sh to read user input for calculations
    Refactored calc.sh to use user input for numbers and operations.

--- calc.sh
+++ calc.sh
@@ -1,16 +1,21 @@
-a = $1
-b = $2
+#!/bin/bash
+
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+
 echo "Enter op:"
 echo "1 -> add"
 echo "2 -> sub"
 echo "3 -> mul"
 echo "4 -> div"
-op = $3
+read op
 
 case ${op} in
-  1)exp =`expr $1 + $2` ;;
-  2)exp =`expr $1 - $2` ;;
-  3)exp =`expr $1 * $2` ;;
-  4)exp =`expr $1 / $2`
+  1) exp=$((a + b)) ;;
+  2) exp=$((a - b)) ;;
+  3) exp=$((a * b)) ;;
+  4) exp=$((a / b)) ;;
+  *) echo "Invalid option"; exit 1 ;;
 esac
+
 echo "ans ${exp}"
