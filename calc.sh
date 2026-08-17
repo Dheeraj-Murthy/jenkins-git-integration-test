b10771024700acc0757608d620271424a042090d
M S Dheeraj Murthy <136881238+Dheeraj-Murthy@users.noreply.github.com>
2026-08-17 09:16:42 +0530

    Modify calc.sh to use command line arguments
    Refactor calc.sh to accept command line arguments for numbers and operation instead of prompting for input.

--- calc.sh
+++ calc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -p "Enter first number: " a
-read -p "Enter second number: " b
+a = $1
+b = $2
 
-echo "Enter op:"
-echo "1 -> add"
-echo "2 -> sub"
-echo "3 -> mul"
-echo "4 -> div"
-read op
+# echo "Enter op:"
+# echo "1 -> add"
+# echo "2 -> sub"
+# echo "3 -> mul"
+# echo "4 -> div"
+op = $3
 
 case ${op} in
   1) exp=$((a + b)) ;;
