c1083c8962d0f48643238a792e00021c8e98782d
M S Dheeraj Murthy <136881238+Dheeraj-Murthy@users.noreply.github.com>
2026-08-17 08:56:04 +0530

    Add basic calculator functionality in calc.sh
    Implement basic calculator operations for addition, subtraction, multiplication, and division.

---
+++ calc.sh
@@ -0,0 +1,15 @@
+a = $1
+b = $2
+echo "Enter op:"
+echo "1 -> add"
+echo "2 -> sub"
+echo "3 -> mul"
+echo "4 -> div"
+op = $3
+
+case $op in
+  1)exp =`expr $1 + $2`
+  2)exp =`expr $1 - $2`
+  3)exp =`expr $1 * $2`
+  4)exp =`expr $1 / $2`
+echo "ans {$exp}"
