25877b2fc204e44cc0e87eaeb6cd69b07b6d3a2c
M S Dheeraj Murthy <136881238+Dheeraj-Murthy@users.noreply.github.com>
2026-08-17 09:18:59 +0530

    Correct variable assignments for parameters
    Fix variable assignment syntax in calc.sh

--- calc.sh
+++ calc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-a = $1
-b = $2
+a=$1
+b=$2
 
 # echo "Enter op:"
 # echo "1 -> add"
 # echo "2 -> sub"
 # echo "3 -> mul"
 # echo "4 -> div"
-op = $3
+op=$3
 
 case ${op} in
   1) exp=$((a + b)) ;;
